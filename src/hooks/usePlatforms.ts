e6d1bd1d170e448233c075f9c0efe19055d91494
AiridasZal <air.airidas1@gmail.com>
2023-04-20 12:25:37 +0300

    Refactoring: Fetching data using React Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,21 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
-interface Platform{
+export interface Platform{
     id: number;
     name: string;
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient
+    .get<FetchResponse<Platform>>('/platforms/lists/parents')
+    .then(res => res.data),
+    staleTime: 7 * 24 * 60 * 60 * 1000, // 1W\
+    initialData: {count: platforms.length, results: platforms}
+})
 
 export default usePlatforms
