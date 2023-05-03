22470eda7164685de34070563a72ef89734ee83c
Airidas Žaliauskas <air.airidas1@gmail.com>
2023-05-04 00:20:21 +0300

    Implementation of game detail fetching

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, transition } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface Props {
@@ -7,7 +7,10 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden" width="100%">
+    <Box borderRadius={10} overflow="hidden" width="100%" _hover={{
+      transform: 'scale(1.03)',
+      transition: 'transform .15s ease-in'
+    }}>
       {children}
     </Box>
   );
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-export interface FetchResponse<T>{
+export interface FetchResponse<T> {
     count: number;
     results: T[];
     next: string | null;
@@ -22,8 +22,12 @@ class APIClient<T> {
 
     getAll(config: AxiosRequestConfig) {
         return axiosInstance
-        .get<FetchResponse<T>>(this.endpoint, config)
-        .then(res => res.data);
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then(res => res.data);
+    }
+
+    get = (id: string | number) => {
+        return axiosInstance.get<T>(this.endpoint + '/' + id).then(res => res.data)
     }
 }
 
