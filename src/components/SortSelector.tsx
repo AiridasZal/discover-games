a682fc46f2fab3a2ad8910e51b98cf9d3bf985b5
Airidas Žaliauskas <air.airidas1@gmail.com>
2023-05-04 17:17:35 +0300

    Fixed sorting orders

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -7,8 +7,8 @@ const SortSelector = () => {
     { value: "", label: "Relevance" },
     { value: "-added", label: "Date added" },
     { value: "name", label: "Name" },
-    { value: "-released", label: "Release date" },
-    { value: "metacritic", label: "Popularity" },
+    { value: "released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
     { value: "-rating", label: "Average rating" },
   ];
 
