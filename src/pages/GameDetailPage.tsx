bd3f22956b02f803be44f695ebc4183c4b4abf93
Airidas Žaliauskas <air.airidas1@gmail.com>
2023-05-04 13:41:44 +0300

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
-import React from 'react'
+import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+import ExpandableText from '../components/expandableText';
+import GameAttributes from '../components/GameAttributes';
 import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
@@ -13,7 +14,8 @@ const GameDetailPage = () => {
     return (
         <>
             <Heading>{game.name}</Heading>
-            <Text>{game.description_raw}</Text>
+            <ExpandableText>{game.description_raw}</ExpandableText>
+            <GameAttributes game={game} />
         </>
     )
 }
